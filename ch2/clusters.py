961ef81ee471e9a58481c8023e4ef3e6330d3630
Changmin Choi <koreaccm@gmail.com>
2013-04-29 02:59:10 +0900

    =

--- ch2/clusters.py
+++ ch2/clusters.py
@@ -112,5 +112,50 @@ def printclust(clust, labels = None, n = 0):
     if clust.left != None: printclust(clust.left, labels = labels, n = n + 1)
     if clust.right != None: printclust(clust.right, labels = labels, n = n + 1)
     
-                        
+from PIL import Image, ImageDraw
+
+def getheight(clust):
+    if clust.left == None and clust.right == None: return 1
+    
+    return getheight(clust.left) + getheight(clust.right)
+
+def getdepth(clust):
+    if clust.left == None and clust.right == None: return 0
+    
+    return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+def drawdendrogram(clust, labels, jpeg='clusters.jpg'):
+    h = getheight(clust) * 20
+    w = 1200
+    depth = getdepth(clust)
+    
+    scaling = float(w - 150) / depth
+    
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    
+    draw.line((0, h/2, 10, h/2), fill = (255, 0, 0))
+    
+    drawnode(draw, clust, 10, (h/2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+    
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = getheight(clust.left) * 20
+        h2 = getheight(clust.right) * 20
+        top = y - (h1 +h2 ) / 2
+        bottom = y + (h1 + h2) / 2  
+        
+        ll =  clust.distance * scaling
+        draw.line((x, top + h1/2, x, bottom - h2/2), fill = (255, 0, 0))
+        draw.line((x, top + h1/2, x + ll, top + h1/2), fill = (255, 0, 0))
+        draw.line((x, bottom - h2/2, x + ll, bottom - h2/2), fill = (255, 0, 0))
+        
+        drawnode(draw, clust.left, x + ll, top + h1/2, scaling, labels)
+        drawnode(draw, clust.right, x + ll, bottom - h2/2, scaling, labels)
+        
+    else:
+        draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+         
+              
                 
